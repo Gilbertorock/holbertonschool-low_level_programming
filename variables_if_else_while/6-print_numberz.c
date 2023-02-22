473f28b193d7e4108487d47d1c5f0f5463dca620
Gilbertorock <gilbert2611@hotmail.com>
2023-02-22 01:08:45 -0500

    Numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+ *  * main - print single digit numbers
+ *   *
+ *    * Description: print single digit numbers
+ *     *
+ *      * Return: Always 0 (Success)
+ *       */
+
 int main(void)
 {
-	char l;
-	for (l = '0'; l <= '9'; l++)
-		putchar(l);
-	putchar('\n');
-	return (0);
+		int i;
+
+			i = 48;
+
+			while (i < 58)
+			{
+				putchar(i);
+				i++;
+			}
+
+			putchar(10);
+
+			return (0);
 }
