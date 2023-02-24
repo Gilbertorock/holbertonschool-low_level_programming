e9db86c9d0bdb4edbff6911703e8ff6b08e4f2c6
Gilberto <6408@holbertonstudents.com>
2023-02-24 07:50:04 -0800

    Numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - print single digit numbers
- *   *
- *    * Description: print single digit numbers
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ * main - print single digit numbers
+ * Description: print single digit numbers
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
