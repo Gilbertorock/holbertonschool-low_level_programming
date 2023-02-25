1d63b71bb2152029280ee12d764c0c0a2c69add5
Gilberto <6408@holbertonstudents.com>
2023-02-25 15:26:56 -0800

    void print_line(int n)

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * prints 10 times the numbers from 0 to 14
- * _putchar three times in your code
- * Return: void
- */
+* more_numbers - prints 10 times the numbers from 0 to 14
+* _putchar three times in your code
+* Return: void
+*/
 
-void more_numbers(void);
+void more_numbers(void)
 {
-		int n, t;
-		
-		for (t = 0; t < 10; t++)
+	int n, t;
+
+	for (t = 0; t < 10; t++)
+	{
+		for (n = 0; n <= 14; n++)
 		{
-			for (n = 0; n <= 14; n++)
+			if (n >= 10)
 			{
-				if (n >= 10)
-				{
-					_putchar((n / 10) + '0');
-				}
-				_putchar((n % 10) + '0');
+				_putchar((n / 10) + '0');
 			}
-			_putchar('n\');
+			_putchar((n % 10) + '0');
 		}
+		_putchar('\n');
+	}
 }
