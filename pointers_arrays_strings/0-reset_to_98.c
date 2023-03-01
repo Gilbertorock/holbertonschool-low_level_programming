08bd4827a9bdb1ed5acadf6584ec7c30b0ed3e78
Gilberto <6408@holbertonstudents.com>
2023-02-28 16:32:58 -0800

    98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - check the code
+ * takes a pointer to an int as parameter
+ * Return: Always 0.
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
