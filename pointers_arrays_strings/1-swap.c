0fb47d7d22375bd41650b2b5986b49a4f3ee3d8e
Gilberto <6408@holbertonstudents.com>
2023-02-28 18:23:42 -0800

    swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,28 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- * Write a function that swaps the values of two integers.
- * This function swaps values pointed by 'a' and 'b'
+ * swap_int - a function that swaps the values of two integers
+ * @a: pointer 1
+ * @b: pointer 2
  * Return: Always 0.
  */
 
 void swap_int(int *a, int *b)
 {
-	int numb = *a;
+	int i = *a;
 	*a = *b;
-	*b = numb;
-}
-
-int main()
-{
-	int a, b,
-	printf("Enter value of a  ");
-	scanf("%d", &a);
-	printf("\n Enter value of b ");
-	scanf("%d", &b);
-	swap(&a, &b);
-	printf("\n After swapping: a = %d, b = %d", a, b);
-	return (0);
+	*b = i;
+	
 }
