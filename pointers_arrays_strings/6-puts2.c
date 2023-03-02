c1b6deb821cc10306541ab1327447a6293eb1ec7
Gilberto <6408@holbertonstudents.com>
2023-03-01 20:05:34 -0800

    starting with the first character

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print alternating chars of string
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int fc;
+
+	fc = 0;
+	while (str[fc] != '\0')
+	{
+		if (fc % 2 == 0)
+			_putchar(str[fc]);
+		fc++;
+	}
+	_putchar('\n');
+}
