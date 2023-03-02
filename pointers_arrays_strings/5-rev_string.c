d6c76d73870f7a9250e2b04026461590a9c3bea4
Gilberto <6408@holbertonstudents.com>
2023-03-01 19:18:53 -0800

    again reverses a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int sc = 0;
+	int n;
+
+	while (s[sc] != '\0')
+		sc++;
+
+	for (n = 0; n < sc; n++)
+	{
+		sc--;
+		rev = s[n];
+		s[n] = s[sc];
+		s[sc] = rev;
+	}
+}
