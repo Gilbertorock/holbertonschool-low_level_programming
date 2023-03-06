d1bb68819eaf7712098722937c4fbba0a2d6a6a0
Gilberto <6408@holbertonstudents.com>
2023-03-05 20:23:39 -0800

    add

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,19 +6,33 @@
  * Return: Always 0 (Success)
  */
 
+#include <stdio.h>
+
+int main(void){
+	
+	int i;
+	
+	for(i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+	
+	return (0);
+}
+/**
+ * Otra solucion con Ascii
 int main(void)
 {
-		int i;
+	int i;
+	i = 48;
 
-			i = 48;
+	while (i < 58)
+	{
+	putchar(i);
+	i++;
+	}
 
-			while (i < 58)
-			{
-				putchar(i);
-				i++;
-			}
+	putchar(10);
 
-			putchar(10);
-
-			return (0);
+	return (0);
+*/
 }
